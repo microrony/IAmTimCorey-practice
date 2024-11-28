f34f4181f448a1587acd3627c4f91a16cf38b81a
Mahabubur Rahman <microronyy@gmail.com>
2024-11-28 09:08:49 +0600

    Greeting Aapplication

---
+++ MySecondProject/GreetingApp/Program.cs
@@ -0,0 +1,15 @@
+﻿// Welcome user to the app.
+Console.WriteLine("Wecome to our Greeting Application");
+Console.WriteLine("This application was built by Rony");
+Console.WriteLine("----------------------------------");
+Console.WriteLine();
+
+// Ask first name.
+Console.Write("What is your first name: ");
+string? firstName;
+firstName = Console.ReadLine();
+
+// Greet them by first name.
+Console.WriteLine();
+Console.WriteLine("Hello " + firstName);
+Console.WriteLine("Thank you for using our Greeting Application");
