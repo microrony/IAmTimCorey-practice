7a0a3c043c290ee2d3ea360f7ff9951f1b3a3b2a
Mahabubur Rahman <microronyy@gmail.com>
2024-12-02 08:45:02 +0600

    build the app

--- StudentCheckApp/StudentCheck/Program.cs
+++ StudentCheckApp/StudentCheck/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+Console.WriteLine("Welcome to our Student Check Application.");
+Console.WriteLine("-----------------------------------------\n");
+
+string? readResult;
+string firstName = "";
+int age;
+
+Console.Write("Please type your first name: ");
+readResult = Console.ReadLine();
+if (readResult != null)
+{
+    firstName = readResult;
+}
+
+Console.Write("Please type your age: ");
+readResult = Console.ReadLine();
+int.TryParse(readResult, out age);
+
+string userName;
+
+if (firstName == "Bob" || firstName == "Sue")
+{
+    userName = $"Professor {firstName}";
+}
+else
+{
+    userName = firstName;
+}
+
+Console.WriteLine($"\nHi {userName},");
+
+if (age < 21)
+{
+    Console.WriteLine($"\nI recommend you to wait {21 - age} years to start this class.");
+} else
+{
+    Console.WriteLine("\nI am happy to let you know, you are ready to start this class.");
+}
+
+Console.WriteLine("\nThank you.");
+
+Console.Write("\nPlease enter to continue.");
+Console.ReadLine();
