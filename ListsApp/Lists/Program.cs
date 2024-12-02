254c29ba01301f807f5e57b2d78b523c61fb1928
Mahabubur Rahman <microronyy@gmail.com>
2024-12-02 13:02:10 +0600

    Lists homework

---
+++ ListsApp/Lists/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+bool noMoreStudents = false;
+string? readResult;
+List<string> classRosterList = new List<string>();
+
+do
+{
+    Console.Clear();
+
+    Console.Write("" +
+        "Please enter the student name you want to add to the class roster list" +
+        "(or type \"no more\" if there are no more students): ");
+    readResult = Console.ReadLine();
+    if (readResult != null)
+    {
+        string studentName = readResult;
+        classRosterList.Add(studentName);
+
+        noMoreStudents = readResult.ToLower() == "no more";
+    }
+
+}
+while (noMoreStudents == false);
+
+Console.WriteLine($"Total {classRosterList.Count} students enrolled");
+
---
+++ ListsApp/Lists/README.md
@@ -0,0 +1,9 @@
+Add students to a class roster List until there are no more students.
+Then print out the count of the students to the Console.
+
+Ask {studentName} and add that to the {classRosterList}
+Please enter the student name you want to add to the class roster list.
+
+When there are no more students type no more to exit the application.
+
+Print the count of the {classRosterList} to the screen.
